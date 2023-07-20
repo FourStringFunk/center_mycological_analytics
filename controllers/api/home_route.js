4bad77fce494c3a1da413d7f7a572e2454ba0b61
Chris Harris <cHarris505@gmail.com>
2023-07-20 11:16:05 -0600

    figuring out handlebars, adding routes for about, getting server setup, etc.

--- controllers/api/home_route.js
+++ controllers/api/home_route.js
@@ -4,12 +4,30 @@ const fs = require('fs')
 
 // '/' endpoint for home.
 router.get('/', async (req, res) => {
-    const mainHeroImg = fs.readFileSync('/img/pexels-artem-podrez-5726794.jpg')
+    // const mainHeroImg = fs.readFileSync('/img/pexels-artem-podrez-5726794.jpg')
     try{
-        res.status(200).render('homepage', { homepage: true, mainHeroImg });
+        res.status(200).render('homepage');
     }catch(error){
         console.error(error);
         res.status(500).send('Server Error');
 }});
+
+// NAVIGATION ROUTERS
+// About page
+router.get('/about', async (req, res) => {
+    try{
+        res.status(200).render('about', {isAboutTemplate});
+    } catch(error) {
+        console.error(error);
+        res.status(500).send('Server Error');
+    };
+});
+
+// Scholarships Page
+
+// Courses Page
     
 module.exports = router;
+
+
+
